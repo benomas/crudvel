a8fd79418808c0859db9f644918d9cfdc6add8e0
Benomas <benomas@gmail.com>
2017-11-28 23:51:39 -0600

    -Se cargan cambios

---
+++ src/customs/Controllers/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Crudvel\Customs\Controllers;
+/**
+ *
+ *
+ * @author Benomas benomas@gmail.com
+ * @date   2017-05-08
+ */
+
+use Crudvel\Controllers\ApiController AS defaultApiController;
+class ApiController extends defaultApiController
+{
+    
+}
---
+++ src/customs/Models/BaseModel.php
@@ -0,0 +1,7 @@
+<?php 
+namespace Crudvel\Customs\Models;
+
+use Crudvel\Models\BaseModel AS defaultModel;
+
+class BaseModel extends defaultModel {
+}
