6ba4f347e89a109d3c76a2d2aab9c2300154fc38
Benomas <benomas@gmail.com>
2018-05-14 02:54:34 -0500

    -Change  default CatPermissionTypes migrations

--- src/database/migrations/leftdatetag_create_cat_permission_types_table_rightdatetag.php
+++ src/database/migrations/leftdatetag_create_cat_permission_types_table_rightdatetag.php
@@ -5,4 +5,20 @@ use Crudvel\Database\Migrations\BaseMigration;
 
 class CreateCatPermissionTypesTablerightdatetag extends BaseMigration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable($this->mainTable)) {
+            Schema::create($this->mainTable, function (Blueprint $table) {
+                $this->catalog($table);
+            });
+            $this->change(function($table){
+                $table->string('slug')->nullable()->after("name");
+            });
+        }
+    }
 }
