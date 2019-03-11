aea5faf35dc1f664191fe097ea131a220e069469
Benomas <benomas@gmail.com>
2019-03-11 17:02:42 -0600

    add modular BaseSeeder version

--- src/database/seeds/BaseSeeder.php
+++ src/database/seeds/BaseSeeder.php
@@ -7,11 +7,15 @@ use Illuminate\Support\Facades\Schema;
 
 class BaseSeeder extends Seeder
 {
-	protected $baseClass;
-	protected $classType = "TableSeeder";
-	protected $crudObjectName;
-	protected $modelSource;
-	protected $model;
+  protected $baseClass;
+  protected $classType = "TableSeeder";
+  protected $crudObjectName;
+  protected $modelSource;
+  protected $model;
+  protected $chunckedSize      = 999;
+  protected $runChunked        = false;
+  protected $enableTransaction = true;
+  protected $deleteBeforeInsert= false;
   use CrudTrait;
   /**+
    * Run the database seeds.
@@ -22,12 +26,40 @@ class BaseSeeder extends Seeder
   {
     if(empty($this->data))
       return false;
+    $this->data = collect($this->data);
     $this->explodeClass();
     disableForeignKeyConstraints();
-    DB::transaction(function() {
+    $modelClass = get_class($this->modelInstanciator(true));
+    if($this->deleteBeforeInsert)
+      $this->modelInstanciator()->delete();
+    if($this->enableTransaction){
+      DB::transaction(function() use($modelClass){
+        if($this->runChunked)
+          $this->chunckedImplementation($modelClass);
+        else
+          $this->defaultImplementation();
+      });
+    }
+    else{
+      if($this->runChunked)
+        $this->chunckedImplementation($modelClass);
+      else
+        $this->defaultImplementation();
+    }
+    enableForeignKeyConstraints();
+  }
+
+  public function chunkSize(){
+    return $this->chunckedSize;
+  }
+
+  public function defaultImplementation(){
     foreach ($this->data as $key => $value)
       $this->modelInstanciator(true)->fill($value)->save();
-    });
-    enableForeignKeyConstraints();
+  }
+
+  public function chunckedImplementation($modelClass){
+    foreach($this->data->chunk($this->chunkSize()) as $subData)
+      $modelClass::insert($subData->toArray());
   }
 }
