4a82862cf5e81106c570714d613c518185f14b36
Benomas <benomas@gmail.com>
2019-03-25 21:56:21 -0600

    ajust logic of CacheTrait

--- src/Traits/CacheTrait.php
+++ src/Traits/CacheTrait.php
@@ -12,10 +12,39 @@ trait CacheTrait
   }
 
   public function getCacheData($property){
+    /*
+    if(isset($GLOBALS['model_cache_'.get_class($this).$property]))
+      return $GLOBALS['model_cache_'.get_class($this).$property];
+    if($callBackResponse = $this->getCallBack($property))
+      return $callBackResponse;
+      */
     if(isset($GLOBALS['model_cache_'.$property]))
       return $GLOBALS['model_cache_'.$property];
     if(isset($this->cacheBoots[$property]))
       return $GLOBALS['model_cache_'.$property] = $this->cacheBoots[$property]();
     return null;
+  }
+
+  public function setCallBack($property,$callBack){
+    $GLOBALS['model_callback_'.get_class($this).$property]=$callBack;
+  }
+
+  public function getCallBack($property){
+    if(
+      isset($GLOBALS['model_callback_'.get_class($this).$property]) &&
+      is_callable($GLOBALS['model_callback_'.get_class($this).$property])
+    )
+      return $GLOBALS['model_cache_'.get_class($this).$property] = $GLOBALS['model_callback_'.get_class($this).$property]();
+    return null;
+  }
+
+  public function setInstanceReady(){
+    $GLOBALS['model_cache_'.get_class($this).'-ready']=true;
+  }
+
+  public function instanceIsReady(){
+    return
+      isset($GLOBALS['model_cache_'.get_class($this).'-ready']) &&
+      $GLOBALS['model_cache_'.get_class($this).'-ready'];
   }
 }
