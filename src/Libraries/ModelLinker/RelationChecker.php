1dd97e9e3fa100e6b8e7fb349f6901e13b307c9d
Energy1011 <energy1011@gmail.com>
2019-04-11 18:02:23 -0500

    Update and Fix RelationChecker, now its working

--- src/Libraries/ModelLinker/RelationChecker.php
+++ src/Libraries/ModelLinker/RelationChecker.php
@@ -34,57 +34,54 @@ class RelationChecker
   }
 
   public function writeRelationCodeInFile($rel, $fileContents, $file, $direction){
+    $old = $fileContents;
     // build the template to insert in the file
     $tpl = $this->buildTpl($rel, $direction);
-    // customLog($tpl);
     // search and replace inside the file
     $fileContents = preg_replace('/(\n\/\/\[*End Relationships\]*)/', "\n" . $tpl . "$1", $fileContents);
     // insert new lines in file
-
-    // pdd($file, $fileContents);
     file_put_contents($file, $fileContents);
-    customLog($fileContents);
+    return $old !== $fileContents;
   }
 
   public function insertRelationInClass($rel, $direction){
     $toggleDirection = ucfirst($this->toggleDirect(lcfirst($direction)));
-    // is this relation I = Ignored ?
-    if($rel[$direction.'Relation'] === 'I') return false;
     $file = base_path().'/'.base64_decode($rel['encoded'.ucfirst($direction).'TargetPath']);
+    // is this relation I = Ignored ?
+    if($rel[$direction.'Relation'] === 'I') return [$file, false, 'Ignored'];
     // open the file to edit contents
     $fileContents = file_get_contents($file);
     $funcName = lcfirst(class_basename(base64_decode($rel['encoded'.$toggleDirection.'Model'])));
     $exist = $this->existRelationCode($fileContents, $funcName);
     if($rel[$direction.'Relation'] === 'F'){
-      if($exist) $fileContents = $this->eraseRelationCode($rel, $direction, $fileContents);
+      if($exist){
+        $fileContents = $this->eraseRelationCode($rel, $direction, $fileContents);
+        if(is_null($fileContents)) return [$file,false, 'Null error eraseRelationCode'];
+        return [$file, $this->writeRelationCodeInFile($rel, $fileContents, $file, $direction),'Forced ok'];
+      }
     }
-    if(!$exist) $this->writeRelationCodeInFile($rel, $fileContents, $file, $direction);
-    return true;
+    if(!$exist) return [$file, $this->writeRelationCodeInFile($rel, $fileContents, $file, $direction)];
+    return [$file, false, 'Default no forced'];
   }
 
   public function eraseRelationCode($rel, $direction, $fileContents){
     $toggleDirection = ucfirst($this->toggleDirect(lcfirst($direction)));
     $funcName = lcfirst(class_basename(base64_decode($rel['encoded'.$toggleDirection.'Model'])));
-    $fileContents = preg_replace('/public\s+function\s+'.$funcName.'\s*\(\).*\n.*return.*\n.*}/',"borrada", $fileContents);
-    if(is_null($fileContents)) pdd("Error eraseRelation preg_replace", $rel, $fileContents);
+    customLog("antes",$fileContents);
+    $fileContents = preg_replace('/public\s+function\s+'.$funcName.'\s*\(\).*\n.*return.*\n.*}/',"", $fileContents);
+    customLog("despues",$fileContents);
     return $fileContents;
   }
 
   public function checkIfRelationsExistInTraits()
   {
+    // Response array to list procced relations
+    $response = [];
     // iter all relations
     foreach ($this->relationArray as $rel) {
-      // check for rightModel (catalogs) exists in left table (details)
-      if (!method_exists($rel['leftModel'], lcfirst(class_basename($rel['rightModel'])))) {
-        // define relationship for lefttable
-        if (!$this->insertRelationInClass($rel,'left')) customLog("No rel inserted: ", $rel);
-      }
-      // check for leftModel (details) exists in left table (catalogs)
-      if (!method_exists($rel['rightModel'], lcfirst(class_basename($rel['leftModel'])))) {
-        // define relationship for rightTable
-        if (!$this->insertRelationInClass($rel,'right')) customLog("No rel inserted: ", $rel);
-      }
+        array_push($response, $this->insertRelationInClass($rel,'left'));
+        array_push($response, $this->insertRelationInClass($rel,'right'));
     }
-      pdd("si se encuentra la relacion");
+    return $response;
   }
 }
