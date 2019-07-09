4fcc9c64d68c80a07c85bf94eccf58c42874f004
benomas <benomas@gmail.com>
2019-07-09 14:09:00 -0500

    add last changes with fast commit

--- src/database/seeds/BaseSeeder.php
+++ src/database/seeds/BaseSeeder.php
@@ -63,7 +63,7 @@ class BaseSeeder extends Seeder
       try{
         $modelClass::insert($subData->toArray());
       }catch(\Exception $e){
-        customLog('Seeder transaction fail with',$subData,json_encode($e));
+        customLog('Seeder transaction fail with',json_encode($subData),json_encode($e));
         cvConsoler(cvRedCoTC("\n".'Exception when running seeder '.json_encode($e)));
       }
     }
