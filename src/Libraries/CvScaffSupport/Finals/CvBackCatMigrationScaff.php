df16dd8f81f789d4bff4ac4a6301aa9945f1f265
Benomas <benomas@gmail.com>
2019-12-29 04:54:49 -0600

    add last changes with fast commit

---
+++ src/Libraries/CvScaffSupport/Finals/CvBackCatMigrationScaff.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Crudvel\Libraries\CvScaffSupport;
+
+use \Crudvel\Interfaces\CvScaffInterface;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class CvBackMigrationScaff extends \Crudvel\Libraries\CvScaffSupport\CvBaseScaff implements CvScaffInterface
+{
+  protected $context='back';
+  public function __construct(){
+    parent::__construct();
+  }
+
+  protected function getTemplatePath(){
+    $fileName = 'cv_back_migration.txt';
+    $path='vendor/benomas/crudvel/src/templates/';
+    return base_path("$path$fileName");
+  }
+
+  protected function getTemplateReceptorPath(){
+    $fileName = Carbon::now()->format('Y_m_d_u').'_create_'.fixedSlug(Str::plural($this->getResource())).'_table.php';
+    $destPath = 'database/migrations/';
+    if($this->isForced()){
+      $migrations = assetsMap(base_path($destPath));
+      foreach((array)$migrations as $migration){
+        $pos=strpos($migration,'_create_'.fixedSlug(Str::plural($this->getResource())).'_table.php');
+        if($pos !== null && $pos !== false){
+          $fileName = $migration;
+          break;
+        }
+      }
+    }
+    return base_path("$destPath$fileName");
+  }
+//[LoadTemplate Modes]
+//[End LoadTemplate Modes]
+
+//[CalculateParams Modes]
+//[End CalculateParams Modes]
+
+//[FixTemplate Modes]
+//[End FixTemplate Modes]
+
+//[InyectFixedTemplate Modes]
+//[End InyectFixedTemplate Modes]
+}
