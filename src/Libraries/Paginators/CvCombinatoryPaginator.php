ab19f98a68139df5a5d2dab0513519842dd35bb3
Benomas <benomas@gmail.com>
2021-02-28 04:21:57 -0600

    fix combinatory-paginator select

--- src/Libraries/Paginators/CvCombinatoryPaginator.php
+++ src/Libraries/Paginators/CvCombinatoryPaginator.php
@@ -102,8 +102,11 @@ class CvCombinatoryPaginator extends CvBasePaginator implements CvPaginate
       $this->getModelBuilderInstance()
       ->orderBy('pt_order',$this->getAscending()==1?"ASC":"DESC")
       ->orderBy($this->getOrderBy(),$this->getAscending()==1?"ASC":"DESC");
+
     if($this->getModelCollectionInstance() && !empty($this->getModelCollectionInstance()->id))
       $this->getModelBuilderInstance()->id($this->getModelCollectionInstance()->id,false);
+
+    $this->getModelBuilderInstance()->select($this->getSelectQuery());
   }
 
   public function filter() {
