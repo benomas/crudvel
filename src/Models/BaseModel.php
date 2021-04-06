13be93a4601087a74674d65da9ccdb2754d4693c
Energy1011 <energy1011@gmail.com>
2021-04-06 04:24:47 -0500

    Update for zipfiles

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -58,5 +58,15 @@ class BaseModel extends Model implements CvCrudInterface
 // [End Scopes]
 
 // [Others]
+  public function fileZipOptPath($resource)
+  {
+    $DS = DIRECTORY_SEPARATOR;
+    return storage_path('app'.$DS.'public'.$DS) . $resource . '-' . $this->getKeyValue() . '-' . time() . '.zip';
+  }
+
+  public function fileZipOptDeleteFileAfterSend()
+  {
+    return true;
+  }
 // [End Others]
 }
