e6339006f2f8e7a47596645c74e7b8fb3171ae84
Benomas <benomas@gmail.com>
2021-10-15 01:47:19 -0500

    add new property exceptionData for exteption validations

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Cache;
 
 abstract class ValidationException extends Exception{
   use \Crudvel\Libraries\Helpers\CasesTrait;
+  protected $exceptionData = [];
 
   abstract protected function getResourceLang():string;
 
+// [Specific Logic]
   public function report(){
   }
 
@@ -19,7 +21,21 @@ abstract class ValidationException extends Exception{
     return \Crudvel\Controllers\ApiController::sApiFailResponse([
       "message"   => __("crudvel/{$this->getResourceLang()}.exceptions.{$this->getErrorType()}").
       " {$this->getMessage()} ",
-      "errorType" => $this->getErrorType()
+      "errorType" => $this->getErrorType(),
+      "exceptionData"=>$this->getExceptionData()
     ]);
   }
+// [End Specific Logic]
+// [Getters]
+  public function getExceptionData(){
+    return $this->exceptionData??null;
+  }
+// [End Getters]
+// [Setters]
+  public function setExceptionData($exceptionData=null){
+    $this->exceptionData = $exceptionData??null;
+
+    return $this;
+  }
+// [End Setters]
 }
