3863754c54e0b3fd89e59bef179be30628ed80d7
Benomas <benomas@gmail.com>
2024-08-14 00:32:49 -0500

    adjust exception message

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -14,8 +14,8 @@ abstract class ValidationException extends Exception {
   public function __construct($message = null, $code = 0, \Exception $previous = null){
     $message = $message ? " - $message":'';
     $message = "{$this->message}{$message}";
+    CustomWarningLog("{$message} Error found at line {$this->getLine()} in {$this->getFile()}: <b>{$this->getMessage()}");
     parent::__construct($message, $code, $previous);
-    $this->message = "Error found at line {$this->getLine()} in {$this->getFile()}: <b>{$this->getMessage()}</b>";
   }
 
 // [Specific Logic]
